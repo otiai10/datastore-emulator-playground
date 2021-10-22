1a0816702fbad56ed297ee3f11b0d0fc01f216da
Hiromu OCHIAI <otiai10@gmail.com>
2021-10-22 20:20:35 +0900

    Add minimum test

---
+++ foo_test.go
@@ -0,0 +1,22 @@
+package datastoreemulatorplayground
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"cloud.google.com/go/datastore"
+	. "github.com/otiai10/mint"
+)
+
+func TestMain(m *testing.M) {
+	code := m.Run()
+	os.Exit(code)
+}
+
+func TestFoo(t *testing.T) {
+	ctx := context.Background()
+	client, err := datastore.NewClient(ctx, os.Getenv("GOOGLE_CLOUD_PROJECT"))
+	Expect(t, client).Not().ToBe(nil)
+	Expect(t, err).ToBe(nil)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/otiai10/datastore-emulator-playground
+
+go 1.16
+
+require (
+	cloud.google.com/go/datastore v1.6.0
+	github.com/otiai10/mint v1.3.3
+)
